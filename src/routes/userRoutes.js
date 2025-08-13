893172bb7c16a693c7b2b58a1bfd0870ff06f1cf
Dzarooo <kacperjaroszewski06@gmail.com>
2025-08-13 20:05:40 +0200

    little chore

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const userController = require('../modules/user/userController');
 const { registerValidation } = require('../modules/user/userValidation');
 const validate = require('../middlewares/validate');
