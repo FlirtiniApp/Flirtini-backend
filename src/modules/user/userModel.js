7097a3e513e02ec57b7d262f0e1a63e6fd7494ac
Dzarooo <kacperjaroszewski06@gmail.com>
2025-08-13 18:59:42 +0200

    added user model

---
+++ src/modules/user/userModel.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+    {
+        login: {
+            type: String,
+            required: [true, 'Login is required'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        firstName: {
+            type: String,
+            required: [true, 'First name is required'],
+        },
+        lastName: {
+            type: String,
+            required: [true, 'Last name is required'],
+        },
+        birthDate: {
+            type: Date,
+            required: [true, 'Birth date is required'],
+        },
+        favouriteDrinks: [
+            {
+                _id: false,
+                drinkId: {
+                    type: Number,
+                    required: false,
+                    default: 0
+                },
+                drinkName: {
+                    type: String,
+                    required: false,
+                    default: ''
+                }
+            }
+        ],
+        friends: {
+            type: [String],
+            required: false,
+            default: []
+        },
+        phoneNumber: {
+            type: String,
+            required: false,
+        },
+        lists: [
+            {
+                _id: false,
+                name: {
+                    type: String,
+                    required: false
+                },
+                drinks: {
+                    type: [Number],
+                    required: false,
+                    default: []
+                }
+            }
+        ]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
