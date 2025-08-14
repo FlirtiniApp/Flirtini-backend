5fff53600594b8c83fde9915f66d16251fa86635
Dzarooo <kacperjaroszewski06@gmail.com>
2025-08-14 02:33:44 +0200

    made seeders

---
+++ src/seeders/seeders.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const seedUsers = require("./userSeeder");
+
+const runSeeders = async () => {
+    // Connect to DB
+    await mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log("\x1b[93mConnected to database\x1b[0m");
+        })
+        .catch((err) => {
+            console.error("\x1b[41mFailed to connect to database\x1b[0m");
+            console.error(err);
+            process.exit(1);
+        });
+
+    // Run seeders
+    const args = process.argv.slice(2);
+
+    if (args.length === 0) {
+        // Call all seeders
+        console.log("Calling all seeders");
+        await seedUsers();
+    }
+    else {
+        if (args.includes("--user")) {
+            console.log("Calling user seeder");
+            await seedUsers();
+        }
+        // Add seeders for future functionalities (e.g. list)
+    }
+
+    // Disconnect from DB
+    await mongoose.disconnect()
+        .then(() => {
+            console.log("\x1b[93mDisconnected from database\x1b[0m");
+        })
+        .catch((err) => {
+            console.error("\x1b[41mFailed to disconnect from database\x1b[0m");
+            console.error(err);
+            process.exit(1);
+        })
+
+    // Finish seeding
+    console.log(`\x1b[35mFinished seeding\x1b[0m`);
+    process.exit();
+}
+
+runSeeders();
---
+++ src/seeders/userSeeder.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const User = require('../modules/user/userModel');
+
+const seedUsers = async () => {
+    try {
+
+        console.log(`\x1b[93mStarting user seeder\x1b[0m`);
+
+        //Clear existing users
+        await User.deleteMany({});
+        console.log("Cleared old users");
+
+        // Insert seed data
+        const users = await User.insertMany([
+            {
+                "login": "admin",
+                "password": "admin",
+                "email": "admin",
+                "firstName": "admin",
+                "lastName": "admin",
+                "birthDate": "2000-01-01T00:00:00.000Z"
+            },
+            {
+                "login": "johndoe",
+                "password": "Johndoe123#",
+                "email": "john@doe.flirtini",
+                "firstName": "John",
+                "lastName": "Doe",
+                "birthDate": "2006-01-14T00:00:00.000Z"
+            },
+            {
+                "login": "jankowalski",
+                "password": "Jankowalski123#",
+                "email": "jan@kowalski.flirtini",
+                "firstName": "Jan",
+                "lastName": "Kowalski",
+                "birthDate": "2006-06-16T00:00:00.000Z"
+            },
+            {
+                "login": "amystaszek",
+                "password": "Amystaszek123#",
+                "email": "amy@staszek.flirtini",
+                "firstName": "Amy",
+                "lastName": "Staszek",
+                "birthDate": "2000-12-06T00:00:00.000Z"
+            },
+        ]);
+
+        console.log(`Inserted ${users.length} users`);
+        console.log(`\x1b[93mFinished user seeder\x1b[0m`);
+    } catch (err) {
+        console.error("\x1b[41mFailed seeding users\x1b[0m");
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+module.exports = seedUsers;
