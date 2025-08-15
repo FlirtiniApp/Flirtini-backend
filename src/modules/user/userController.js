16a2e5b29025060a41cbd3bd4b63815b71e75de7
Dzarooo <kacperjaroszewski06@gmail.com>
2025-08-15 13:57:11 +0200

    added some console logs

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -39,6 +39,7 @@ const register = async (req, res) => {
         console.log(`\x1b[35mSuccesfully registered\x1b[0m`);
         res.status(201).json({ message: "successfully registered", token: token });
     } catch (error) {
+        console.log("\x1b[31mError occurred while trying to register\x1b[0m");
         res.status(500).json({ message: error.message });
     }
 }
@@ -46,23 +47,28 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     try {
         const user = req.body;
-        console.log(user);
 
+        console.log("Searching for user with given credentials...");
         const userDB = await User.findOne({ login: user.login, password: user.password });
-        console.log(userDB);
 
         if (userDB) {
+            console.log("\x1b[93mUser found\x1b[0m");
             const token = jwt.sign(
                 { id: user._id },
                 process.env.JWT_SECRET,
                 { expiresIn: '30d' }
             );
 
+            console.log("\x1b[93mSuccessfully logged in\x1b[0m");
             res.status(200).json({ message: "Successfully logged in", token: token });
         }
-        else res.status(401).json({ message: "User with given login and password does not exist" });
+        else {
+            console.log("\x1b[31mUser not found\x1b[0m");
+            res.status(401).json({ message: "User with given login and password does not exist" });
+        }
 
     } catch (error) {
+        console.log("\x1b[41mError occurred while trying to log in\x1b[0m");
         res.status(500).json({ message: error.message });
     }
 }
